62f4345dee3e73034b0212c177d3a1f95abf1903
GalexVM <80070226+GalexVM@users.noreply.github.com>
2021-04-16 12:11:45 -0500

    Add files via upload

---
+++ Practica manipulacion strings/Pregunta_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include<string>
+using std::string;
+
+int main(){
+	int numero = 15683;
+	string resultado = (string)numero;
+	
+}
